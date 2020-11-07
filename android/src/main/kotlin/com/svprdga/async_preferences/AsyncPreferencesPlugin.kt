26ac3029fd4e31e0d36b76be5e2bfa6a16c87cc7
svprdga <svprdga@tutanota.com>
2020-11-07 13:59:43 +0100

    Persist strings, booleans and ints

--- android/src/main/kotlin/com/svprdga/async_preferences/AsyncPreferencesPlugin.kt
+++ android/src/main/kotlin/com/svprdga/async_preferences/AsyncPreferencesPlugin.kt
@@ -9,28 +9,96 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-/** AsyncPreferencesPlugin */
-class AsyncPreferencesPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+import java.util.*
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "async_preferences")
-    channel.setMethodCallHandler(this)
-  }
+class AsyncPreferencesPlugin : FlutterPlugin, MethodCallHandler {
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    // ****************************************** VARS ***************************************** //
+
+    private lateinit var channel: MethodChannel
+    private lateinit var preferences: Preferences
+
+    // *************************************** LIFECYCLE *************************************** //
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "async_preferences")
+        channel.setMethodCallHandler(this)
+        preferences = Preferences(flutterPluginBinding.getApplicationContext())
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
+        when (call.method) {
+            "set_string" -> setString(call, result)
+            "get_string" -> getString(call, result)
+            "set_bool" -> setBoolean(call, result)
+            "get_bool" -> getBoolean(call, result)
+            "set_int" -> setInt(call, result)
+            "get_int" -> getInt(call, result)
+            else -> result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    // ************************************ PRIVATE METHODS ************************************ //
+
+    private fun setString(call: MethodCall, result: MethodChannel.Result) {
+        try {
+            val list = call.arguments as ArrayList<String>
+            result.success(preferences.setString(list[0], list[1]))
+        } catch (e: Exception) {
+            result.error("set_string_error", e.message, null)
+        }
+    }
+
+    private fun getString(call: MethodCall, result: MethodChannel.Result) {
+        try {
+            val list = call.arguments as ArrayList<String>
+            val value = preferences.getString(list[0])
+            result.success(value)
+        } catch (e: Exception) {
+            result.error("get_string_error", e.message, null)
+        }
+    }
+
+    private fun setBoolean(call: MethodCall, result: MethodChannel.Result) {
+        try {
+            val list = call.arguments as ArrayList<Any>
+            result.success(preferences.setBoolean(list[0] as String, list[1] as Boolean))
+        } catch (e: Exception) {
+            result.error("set_boolean_error", e.message, null)
+        }
+    }
+
+    private fun getBoolean(call: MethodCall, result: MethodChannel.Result) {
+        try {
+            val list = call.arguments as ArrayList<String>
+            val value = preferences.getBoolean(list[0])
+            result.success(value)
+        } catch (e: Exception) {
+            result.error("get_boolean_error", e.message, null)
+        }
+    }
+
+    private fun setInt(call: MethodCall, result: MethodChannel.Result) {
+        try {
+            val list = call.arguments as ArrayList<Any>
+            result.success(preferences.setInt(list[0] as String, list[1] as Int))
+        } catch (e: Exception) {
+            result.error("set_int_error", e.message, null)
+        }
+    }
+
+    private fun getInt(call: MethodCall, result: MethodChannel.Result) {
+        try {
+            val list = call.arguments as ArrayList<String>
+            val value = preferences.getInt(list[0])
+            result.success(value)
+        } catch (e: Exception) {
+            result.error("get_int_error", e.message, null)
+        }
+    }
+
 }
---
+++ android/src/main/kotlin/com/svprdga/async_preferences/Preferences.kt
@@ -0,0 +1,51 @@
+package com.svprdga.async_preferences
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
+
+class Preferences(context: Context) {
+
+    // ****************************************** VARS ***************************************** //
+
+    private val sharedPreferences: SharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(context)
+    private val editor: SharedPreferences.Editor = sharedPreferences.edit()
+    
+    // ************************************* PUBLIC METHODS ************************************ //
+    
+    fun setString(id: String, value: String): Boolean {
+        editor.putString(id, value)
+        return editor.commit()
+    }
+    
+    fun getString(id: String): String? {
+        return sharedPreferences.getString(id, null)
+    }
+
+    fun setBoolean(id: String, value: Boolean): Boolean {
+        editor.putBoolean(id, value)
+        return editor.commit()
+    }
+
+    fun getBoolean(id: String): Boolean? {
+        return if (sharedPreferences.contains(id)) {
+            sharedPreferences.getBoolean(id, false)
+        } else {
+            null
+        }
+    }
+
+    fun setInt(id: String, value: Int): Boolean {
+        editor.putInt(id, value)
+        return editor.commit()
+    }
+
+    fun getInt(id: String): Int? {
+        return if (sharedPreferences.contains(id)) {
+            sharedPreferences.getInt(id, 0)
+        } else {
+            null
+        }
+    }
+}
