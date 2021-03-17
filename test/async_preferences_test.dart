45cbac3232d328c74af3a9804130ee6465fb9d47
svprdga <svprdga@reprom.io>
2021-03-17 17:43:16 +0100

    Migrate package to null safety

--- test/async_preferences_test.dart
+++ test/async_preferences_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:async_preferences/async_preferences.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('async_preferences');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await AsyncPreferences.platformVersion, '42');
   });
 }
