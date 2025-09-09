2bfc5d370da617f95b0a2e7f1be28004f30ab680
svprdga <david@davidserrano.io>
2025-09-09 13:16:39 +0200

    temp

--- android/src/main/kotlin/com/svprdga/async_preferences/PreferencesDataStore.kt
+++ android/src/main/kotlin/com/svprdga/async_preferences/PreferencesDataStore.kt
@@ -1,10 +1,8 @@
 package com.svprdga.async_preferences
 
 import android.content.Context
-import android.util.Log
-import androidx.datastore.core.DataMigration
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.SharedPreferencesMigration as PreferencesMigration
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
@@ -17,74 +15,22 @@ import kotlinx.coroutines.flow.map
 
 class PreferencesDataStore(
     private val context: Context,
-    private val keysToMigrate: List<String>,
-    name: String? = null,
+    private val name: String? = null,
+    private val keysToMigrate: List<String>
 ) {
 
     private val dataStoreName = name ?: "${context.packageName}_preferences"
 
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
         name = dataStoreName,
-        produceMigrations = { context ->
-            listOf(object: DataMigration<Preferences> {
-                override suspend fun shouldMigrate(currentData: Preferences) = true
-//                override suspend fun shouldMigrate(currentData: Preferences): Boolean {
-//                    val sharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE)
-//                    return keysToMigrate.any { key ->
-//                        sharedPreferences.contains(key) && !currentData.contains(stringPreferencesKey(key)) &&
-//                        !currentData.contains(booleanPreferencesKey(key)) &&
-//                        !currentData.contains(intPreferencesKey(key)) &&
-//                        !currentData.contains(longPreferencesKey(key))
-//                    }
-//                }
-
-                override suspend fun migrate(currentData: Preferences): Preferences {
-                    val out = currentData.toMutablePreferences()
-                    Log.d("DS-MIG", "Initial=$out")
-
-                    val sharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE)
-
-                    val allValues = sharedPreferences.all
-
-                    // TODO
-                    Log.d("DS-MIG", "SP keys=${allValues}")
-                    Log.d("DS-MIG", "keysToMigrate=$keysToMigrate")
-
-                    keysToMigrate.forEach { key ->
-                        val value = allValues[key]
-                        
-                        when (value) {
-                            is String -> out[stringPreferencesKey(key)] = value
-                            is Boolean -> out[booleanPreferencesKey(key)] = value
-                            is Int -> out[intPreferencesKey(key)] = value
-                            is Long -> out[longPreferencesKey(key)] = value
-                        }
-                    }
-
-                    // TODO
-                    Log.d("DS-MIG", "Migrated=$out")
-
-
-                    return out
-                }
-
-                override suspend fun cleanUp() {
-                    val sharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE)
-                    val editor = sharedPreferences.edit()
-                    keysToMigrate.forEach { key ->
-                        editor.remove(key)
-                    }
-                    editor.apply()
-                }
-            })
-
-
-//            listOf(
-//                SharedPreferencesMigration(
-//                    ctx,
-//                    dataStoreName
-//                )
-//            )
+        produceMigrations = { ctx ->
+            listOf(
+                PreferencesMigration(
+                    ctx,
+                    dataStoreName,
+                    keysToMigrate.toSet()
+                )
+            )
         }
     )
 
